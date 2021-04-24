07a0f3bf1f4342c50ce709890c251baba2144adf
Vanessa Sinoki <75032280+Sinoki@users.noreply.github.com>
2021-04-24 11:06:40 -0300

    part-2
    • Responsivo;
    • Alinhamento dos menus;
    • Dropdown no menu Games;
    • Acesso ao conteúdo do menu about;
    • Navbar.

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,34 @@
 <template>
-  <div class="marc">
-    <div class="title">Sinoki Card Games</div>
-  </div>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/games">Games</router-link> |
-    <router-link to="/login">Login</router-link>
-  </div>
+  <Navbar />
   <router-view />
 </template>
 
+<script>
+import { defineComponent } from 'vue';
+
+import Navbar from '@/components/Navbar.vue';
+
+export default defineComponent({
+  components: { Navbar },
+});
+</script>
+
 <style lang="scss">
-.marc {
-  background: lightcoral;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
-.title:hover {
-  color: white;
-}
-.title {
+
+body {
   font-family: "Comic Sans MS";
-  font-style: italic;
-  border-radius: 10px;
-  cursor: pointer;
-  font-size: 30px;
-  color: black;
-}
-#app {
-  font-family: 'Courier New', Courier, monospace;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  background-color: lightgreen;
+  background-color: black;
+  color: silver;
 }
 
-#nav {
-  padding: 30px;
-  font-size: 25px;
-
-  a {
-    font-weight: bold;
-    color: grey;
-
-    &.router-link-exact-active {
-      color: seagreen;
-    }
-  }
-}
-
-.flex {
-  display: flex;
-}
-
-.flex-wrap {
-  flex-wrap: wrap;
+header {
+  width: 100vw;
+  background-color: #222;
+  padding: 15px;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="title">Sinoki Card Games</div>
+  <nav>
+    <div class="menu-item"><router-link to="/">Home</router-link></div>
+    <div class="menu-item"><router-link to="/about">About</router-link></div>
+    <Dropdown title="Games" :items="games" />
+    <div class="menu-item"><router-link to="/">Login</router-link></div>
+    <div class="menu-item">
+      <router-link to="/">Contact</router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+import Dropdown from './Dropdown.vue';
+
+export default {
+  name: 'navbar',
+  components: {
+    Dropdown,
+  },
+  data() {
+    return {
+      games: [
+        {
+          title: 'Pokémon',
+          link: '#',
+        },
+        {
+          title: 'One Punch man',
+          link: '#',
+        },
+        {
+          title: 'Yu Yu Hakusho',
+          link: '#',
+        },
+        {
+          title: 'Naruto',
+          link: '#',
+        },
+        {
+          title: 'Death Note',
+          link: '#',
+        },
+        {
+          title: 'Dragon Ball Z',
+          link: '#',
+        },
+        {
+          title: 'One Piece',
+          link: '#',
+        },
+        {
+          title: 'Attack on Titan',
+          link: '#',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+    <style>
+.title:hover {
+  color: white;
+}
+.title {
+  font-family: "Comic Sans MS";
+  color: black;
+  font-size: 30px;
+  background: lightcoral;
+  display: flex;
+  justify-content: center;
+}
+
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+nav .menu-item {
+  color: #fff;
+  padding: 10px 20px;
+  position: relative;
+  text-align: center;
+  border-bottom: 3px solid transparent;
+  display: flex;
+  transition: 0.4s;
+}
+
+nav .menu-item.active,
+nav .menu-item:hover {
+  background-color: #444;
+  border-bottom-color: #ff5858;
+}
+
+nav .menu-item a {
+  color: inherit;
+  text-decoration: none;
+}
+</style>
