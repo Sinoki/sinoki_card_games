6b3811d7df5e4d77ad0b10645c08cae54922a79a
Vanessa Sinoki <75032280+Sinoki@users.noreply.github.com>
2021-06-24 22:34:51 -0300

    message

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,15 +6,18 @@
 
     <div class="container">
       <div data-am-fadeshow="next-prev-navigation">
+        <!-- Radio -->
         <input type="radio" name="css-fadeshow" id="slide-1" />
         <input type="radio" name="css-fadeshow" id="slide-2" />
         <input type="radio" name="css-fadeshow" id="slide-3" />
 
+        <!-- Slides -->
         <div class="fs-slides">
           <div
             class="fs-slide"
             style="background-image: url(https://i.ibb.co/ZHF7SyQ/batalha.jpg)"
-          ></div>
+          >
+          </div>
           <div
             class="fs-slide"
             style="background-image: url(https://i.ibb.co/k44tdZt/pokemon.jpg)"
@@ -26,16 +29,22 @@
             "
           ></div>
         </div>
+
+        <!-- Quick Navigation (navegação rápida) -->
         <div class="fs-quick-nav">
           <label class="fs-quick-btn" for="slide-1"></label>
           <label class="fs-quick-btn" for="slide-2"></label>
           <label class="fs-quick-btn" for="slide-3"></label>
         </div>
+
+        <!-- Prev Navigation (Navegação anterior) -->
         <div class="fs-prev-nav">
           <label class="fs-prev-btn" for="slide-1"></label>
           <label class="fs-prev-btn" for="slide-2"></label>
           <label class="fs-prev-btn" for="slide-3"></label>
         </div>
+
+        <!-- Next Navigation -->
         <div class="fs-next-nav">
           <label class="fs-next-btn" for="slide-1"></label>
           <label class="fs-next-btn" for="slide-2"></label>
@@ -47,7 +56,7 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
   setup() {
@@ -63,6 +72,7 @@ export default defineComponent({
   font-size: 27px;
   color: #f9105a;
 }
+
 $max-slides: 5;
 $prefix: true;
 $responsive: true;
@@ -81,6 +91,8 @@ $quick-nav-active-color: #f9105a;
 $fade-color: black;
 $autoplay: true;
 $autoplay-duration: 5;
+
+/* Mixins to handle prefixes (Mixins para lidar com prefixos)*/
 @mixin fadeshow-transition($value...) {
   @if length($value) < 1 {
     $value: all 150ms linear;
@@ -90,6 +102,7 @@ $autoplay-duration: 5;
   }
   transition: $value;
 }
+
 @mixin fadeshow-transform($value...) {
   @if length($value) < 1 {
     $value: all 150ms linear;
@@ -100,18 +113,21 @@ $autoplay-duration: 5;
   }
   transform: $value;
 }
+
 @mixin fadeshow-animation($value...) {
   @if $prefix == true {
     -webkit-animation: $value;
   }
   animation: $value;
 }
+
 @mixin fadeshow-animation-delay($value) {
   @if $prefix == true {
     -webkit-animation-delay: $value;
   }
   animation-delay: $value;
 }
+
 @mixin fadeshow-unselectable() {
   @if $prefix == true {
     -webkit-user-select: none;
@@ -120,6 +136,8 @@ $autoplay-duration: 5;
   }
   user-select: none;
 }
+
+/* Mixins to handle Quick Navigation Button states(Mixins para lidar com os estados do botão de navegação rápida) */
 @mixin quickNavBtnActive() {
   background-color: $quick-nav-active-color;
 }
@@ -127,11 +145,15 @@ $autoplay-duration: 5;
 @mixin quickNavBtnInactive() {
   background-color: $quick-nav-color;
 }
+
+/* Base functionality for the fadeshow. (Funcionalidade básica para fadeshow)*/
 [data-am-fadeshow] {
   height: auto;
   width: 100%;
   overflow: hidden;
   background-color: $fade-color;
+
+  /* Slides */
   .fs-slide {
     position: absolute;
     top: 95px;
@@ -140,14 +162,19 @@ $autoplay-duration: 5;
     right: 0;
     height: 340px;
     opacity: 0;
+    border-radius: 10px;
     background-size: contain;
     background-position: center;
     background-repeat: no-repeat;
     @include fadeshow-transition(opacity #{$fade-time}s ease);
+
+    /* Display first child on load(Exibir o primeiro filho no carregamento) */
     &:first-child {
       opacity: 1;
     }
   }
+
+  /* Quick Navigation */
   .fs-quick-nav {
     position: absolute;
     bottom: 40px;
@@ -156,6 +183,7 @@ $autoplay-duration: 5;
     @include fadeshow-unselectable();
     @include fadeshow-transform(translateX(-50%));
   }
+
   .fs-quick-btn {
     display: inline-block;
     width: 15px;
@@ -165,13 +193,18 @@ $autoplay-duration: 5;
     cursor: pointer;
     @include fadeshow-transition(opacity 200ms ease);
     @include quickNavBtnInactive();
+
     &:hover {
       opacity: 0.8;
     }
+
+    /* Set active state to first button on load(Define o estado ativo para o primeiro botão no carregamento) */
     &:first-child {
       @include quickNavBtnActive();
     }
   }
+
+  /* Prev/Next Navigation */
   %prev-next {
     position: absolute;
     display: none;
@@ -184,17 +217,20 @@ $autoplay-duration: 5;
     background-color: $prev-next-bg;
     -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
     @include fadeshow-transition(all 200ms ease);
+
     @if $responsive == true {
       @media (max-width: $breakpoint-small) {
         width: 50%;
       }
     }
+
     &:before {
       position: absolute;
       top: 50%;
       font-size: $prev-next-icon-size;
       color: $prev-next-icon-color;
     }
+
     &:hover {
       background-color: $prev-next-bg-hover;
       @if $responsive == true {
@@ -204,15 +240,23 @@ $autoplay-duration: 5;
       }
     }
   }
+
+  /* Base functionality */
   input[type="radio"] {
     position: fixed;
     top: -9999px;
+
     &:checked {
+      /* This loop handles the slide switching and quick navigation active state
+      (Este loop lida com a troca de slides e o estado ativo de navegação rápida) */
       $i: $max-slides;
+
       @while $i > 0 {
         &:nth-of-type(#{$i}) {
+          /* Show slide */
           ~ .fs-slides {
             .fs-slide {
+              //Cancel autoplay
               opacity: 0;
               @include fadeshow-animation(none !important);
             }
@@ -220,9 +264,12 @@ $autoplay-duration: 5;
               opacity: 1;
             }
           }
+
+          /* Add active state to dot(Adicionar estado ativo ao ponto) */
           ~ .fs-quick-nav {
             .fs-quick-btn {
               @include quickNavBtnInactive();
+              /* Cancel autoplay */
               @include fadeshow-animation(none !important);
             }
             .fs-quick-btn:nth-child(#{$i}) {
@@ -235,6 +282,8 @@ $autoplay-duration: 5;
         }
         $i: $i - 1;
       }
+
+      /* Hide navigation if only one slide(Ocultar navegação se apenas um slide) */
       &:first-of-type:last-of-type {
         ~ .fs-quick-nav,
         ~ .fs-prev-nav,
@@ -244,6 +293,8 @@ $autoplay-duration: 5;
       }
     }
   }
+
+  /* Autohide Prev/Next Navigation(Ocultar automaticamente a navegação anterior / posterior) */
   @if $autohide-next-prev == true {
     &:hover {
       .fs-prev-btn,
@@ -253,14 +304,19 @@ $autoplay-duration: 5;
     }
   }
 }
+
+/* Handle Prev/Next buttons(Lidar com os botões Anterior / Próximo) */
 [data-am-fadeshow~="next-prev-navigation"] {
   input[type="radio"] {
     &:checked {
       $i: $max-slides;
+
       @while $i > 0 {
         &:nth-of-type(#{$i}) {
           $prev: ($i - 1);
           $next: ($i + 1);
+
+          /* Show previous button(Mostrar botão anterior) */
           @if $prev > 0 {
             ~ .fs-prev-nav {
               .fs-prev-btn {
@@ -271,6 +327,8 @@ $autoplay-duration: 5;
               }
             }
           }
+
+          /* Show next button */
           @if $next < ($max-slides + 1) {
             ~ .fs-next-nav {
               .fs-next-btn {
@@ -281,6 +339,8 @@ $autoplay-duration: 5;
               }
             }
           }
+
+          /* Show last prev button if first slide(Mostrar o último botão anterior do primeiro slide) */
           &:first-of-type {
             ~ .fs-prev-nav {
               .fs-prev-btn:last-child {
@@ -288,6 +348,8 @@ $autoplay-duration: 5;
               }
             }
           }
+
+          /* Show first next button if last slide(Mostrar o primeiro botão seguinte do último slide)*/
           &:last-of-type {
             ~ .fs-next-nav {
               .fs-next-btn:first-child {
@@ -300,17 +362,32 @@ $autoplay-duration: 5;
       }
     }
   }
+
+  /* Display first pair of Prev/Next Buttons on load(Exibir o primeiro par de botões Anterior / Próximo ao carregar) */
   .fs-prev-btn:last-child {
     display: block;
   }
+
   .fs-next-btn:nth-child(2) {
     display: block;
   }
 }
+
+/*
+ * ===== AUTOPLAY (Reprodução automática) =====
+ *
+ * Functions to handle autoplay.(Funções para lidar com a reprodução automática.)
+ * If you're not going to use autoplay, set $autoplay to false.(Se você não for 
+ usar a reprodução automática, defina $ autoplay como falso.)
+ * This part tends to compile into a lot of uneccesary CSS otherwise.(Esta parte 
+ tende a compilar em muitos CSS desnecessários de outra forma.)
+ */
 @if $autoplay == true {
   $i: $max-slides;
+
   @while $i > 1 {
     $total-duration: $autoplay-duration * $i;
+
     @keyframes quickNavAnimation-#{$i} {
       0% {
         @include quickNavBtnInactive();
@@ -339,6 +416,7 @@ $autoplay-duration: 5;
         }
       }
     }
+
     @keyframes slidesAnimation-#{$i} {
       0% {
         opacity: 0;
@@ -367,8 +445,10 @@ $autoplay-duration: 5;
         }
       }
     }
+
     [data-am-fadeshow~="autoplay"] {
       input[type="radio"]:nth-of-type(#{$i}):last-of-type {
+        /* Slide animation */
         ~ .fs-slides {
           .fs-slide {
             @include fadeshow-animation(
@@ -376,6 +456,8 @@ $autoplay-duration: 5;
             );
           }
         }
+
+        /* Quick navigation animation */
         ~ .fs-quick-nav {
           .fs-quick-btn {
             @include fadeshow-animation(
@@ -383,6 +465,8 @@ $autoplay-duration: 5;
             );
           }
         }
+
+        /* Animation Delay */
         @for $j from 1 through $i {
           ~ .fs-slides .fs-slide:nth-child(#{$j}),
           ~ .fs-quick-nav .fs-quick-btn:nth-child(#{$j}) {
@@ -395,6 +479,8 @@ $autoplay-duration: 5;
     }
   }
 }
+
+/* Base Styling */
 .container {
   position: absolute;
   top: 170px;
